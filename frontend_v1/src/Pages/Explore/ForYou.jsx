c872152a4b8870a73fda734030c517c1fff03fbd
Mrrupendra <rupendrasingh112233@gmail.com>
2021-11-13 18:50:21 +0530

    updated

--- frontend_v1/src/Pages/Explore/ForYou.jsx
+++ frontend_v1/src/Pages/Explore/ForYou.jsx
@@ -1,21 +1,27 @@
 import BasicTabs from "../../Components/Explore_Components/LinkTab";
-import { Navbar } from "../../Components/Navbar/Navbar";
+import { SideBar } from "../../Components/Explore_Components/SideBar";
+// import { Navbar } from "../../Components/Navbar/Navbar";
 import styles from "./ForYou.module.css"
 
 function ForYou () {
     return (
+        <>
+        {/* <Navbar page = {"home"} /> */}
         <div className={styles["mainFor"]}>
-            <Navbar page = {"home"} />
+            
             <div className={styles["mainSubFor"]}>
                 <div className={styles["mainSubFor_II"]}> 
                     <div className={styles["mainSubFor_III"]}>
                         <BasicTabs />
                     </div>
                 </div>
-                <div> </div>
+                <div>
+                    <SideBar />
+                </div>
             </div>
 
         </div>
+        </>
     )
 }
 
